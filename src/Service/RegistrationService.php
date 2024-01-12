619fd0827f464847499c707637a88c4aa2764e2a
tomaszkedziora14 <tomaszkedziora14@wp.pl>
2024-01-12 11:59:34 +0100

    creating an separate service (SOLID) for S

---
+++ src/Service/RegistrationService.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Service;
+
+use App\Entity\User;
+use App\Repository\UserRepository;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
+class RegistrationService
+{
+    private $userRepository;
+    private $passwordHasher;
+
+    public function __construct(UserRepository $userRepository, UserPasswordHasherInterface $passwordHasher)
+    {
+        $this->userRepository = $userRepository;
+        $this->passwordHasher = $passwordHasher;
+    }
+
+    public function registerUser(User $user): bool
+    {
+        $existingUser = $this->userRepository->findByUserEmail($user->getEmail());
+        if ($existingUser) {
+            return false;
+        }
+
+        $hashedPassword = $this->passwordHasher->hashPassword($user, $user->getPassword());
+
+        $user->setPassword($hashedPassword)->setRoles($user->getRoles());
+
+        $this->userRepository->save($user);
+
+        return true;
+    }
+}
