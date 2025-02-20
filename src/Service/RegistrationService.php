2e8bdb59c3549fd87109629b84844ebf871905a2
devToms <tomaszkedziora14@wp.pl>
2025-02-20 16:57:42 +0100

    Update RegistrationService.php

--- src/Service/RegistrationService.php
+++ src/Service/RegistrationService.php
@@ -3,20 +3,16 @@
 namespace App\Service;
 
 use App\Entity\User;
-use App\Repository\UserRepository;
+use App\Repository\UserRepositoryInterface;
 use App\Service\RegistrationServiceInterface;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class RegistrationService implements RegistrationServiceInterface
 {
-    private UserRepository $userRepository;
-    private UserPasswordHasherInterface $passwordHasher;
-
-    public function __construct(UserRepository $userRepository, UserPasswordHasherInterface $passwordHasher)
-    {
-        $this->userRepository = $userRepository;
-        $this->passwordHasher = $passwordHasher;
-    }
+    public function __construct(
+        private UserRepositoryInterface $userRepository,
+        private UserPasswordHasherInterface $passwordHasher
+    ) {}
 
     public function registerUser(User $user): bool
     {
@@ -26,9 +22,7 @@ class RegistrationService implements RegistrationServiceInterface
         }
 
         $hashedPassword = $this->passwordHasher->hashPassword($user, $user->getPassword());
-
         $user->setPassword($hashedPassword)->setRoles($user->getRoles());
-
         $this->userRepository->save($user, true);
 
         return true;
